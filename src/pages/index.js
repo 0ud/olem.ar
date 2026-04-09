69a9ee1a1fca25e40d05af00dbaff3405f7e8af7
mohamed <mhyahoo2006@gmail.com>
2026-04-10 00:43:41 +0200

    Add initial Arabic documentation for Python programming concepts
    - Created documentation files for various topics including:
      - Introduction to Python
      - Setting up the programming environment
      - Writing the first line of code
      - Variables and data types
      - Functions
      - Conditional statements
      - Loops
      - User input
      - Exercises

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -21,7 +22,6 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       description="Description will go into a meta tag in <head />">
